39f1c5d9c14d52570b0cd55ba658bb07e1b4c75a
a25patel <aanaletap2510@gmail.com>
2016-09-26 16:03:37 -0600

    day 5 rule two working

--- day05/app.js
+++ day05/app.js
@@ -1,62 +1,72 @@
 // 31 is too low, 274 is too high
-
+// Nice Rules:
+// 1. Must have double letter (aa, bb)
+// 2. Must have at least 3 vowels
+// 3. Must not contain phrases:
+//    ab, cd, pq, or xy
 var fs = require('fs');
 var input = fs.readFileSync('./practicePuzzle.txt', 'utf8');
 var lines = input.split('\n');
-
-// looking for nice lines rules:
-// 1. must have double letter (aa, bb)
-// 2. must have at least 3 vowels
-// 3. must not contain phrases:
-//    ab, cd, pq, or xy
-
-var niceLinesCount = 0;
-console.log(lines);
 
 // Test One: Does it have double letters?
 function doubles(line){
   line = line.split('');
   for (var i = 0; i < line.length; i++) {
     if (line[i] === line[i + 1]) {
-      return ("TRUE" + line[i] + line[i+1]);
+      return line.join('');
     }
   }
 }
 
-console.log(doubles('rthkunfaakmwmush'));
+lines = lines.filter(doubles);
+console.log(lines);
 
-Test Two: Has atleast 3 Vowels
+// // Test Two: Has atleast 3 Vowels
+function vowels(line){
+  line = line.split('');
+  vowelCount = 0;
+  for (var i = 0; i < line.length; i++) {
+    if (line[i] === 'a') {
+      vowelCount ++;
+    }
+    if (line[i] === 'e') {
+      vowelCount ++;
+    }
+    if (line[i] === 'i') {
+      vowelCount ++;
+    }
+    if (line[i] === 'o') {
+      vowelCount ++;
+    }
+    if (line[i] === 'u') {
+      vowelCount ++;
+    }
+  }
+  if (vowelCount >= 3) {
+    return line.join('')
+  }
+}
 
-
-
-//
-// // Test One:
-// // does it have 3 vowels?
-// function vowelChecker(line) {
-//   'use strict';
-//   var vowelChecker = 0;
-//
-//   for (var n = 0; n < line.length; n++) {
-//     switch (line[n]) {
+lines = lines.filter(vowels);
+console.log(lines);
+// function vowels(line){
+//   var vowelCounter = 0;
+//   line = line.split('');
+//   for (var i = 0; i < line.length; i++) {
+//     switch (line[i]) {
 //       case 'a':
-//       case 'e':
-//       case 'i':
-//       case 'o':
-//       case 'u':
-//         vowelChecker++;
+//
 //         break;
+//       default:
+//
 //     }
-//   }
 //
-//   // if vowel count < 3, line is
-//   // not eligible for nice list
-//   if (vowelChecker < 3) {
-//     return false;
+//   if (vowelCounter >= 3) {
+//     return "TRUE";
 //   }
-//
-//   return true;
 // }
 //
+// console.log(vowels('rthkunfaakmwmush'));
 
 // // Test Three:
 // // can't contain certain phrases
