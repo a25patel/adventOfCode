473dbd524e53bca435fc56891e158710a7df994c
a25patel <aanaletap2510@gmail.com>
2016-09-27 13:26:05 -0600

    rule two of part two working

--- day05/app.js
+++ day05/app.js
@@ -7,8 +7,9 @@
 // Answer: 258
 
 var fs = require('fs');
-var input = fs.readFileSync('./puzzle.txt', 'utf8');
+var input = fs.readFileSync('./practicePuzzle.txt', 'utf8');
 var lines = input.split('\n');
+var lines2 = input.split('\n');
 
 // Test One: Does it have double letters?
 function doubles(line){
@@ -70,5 +71,38 @@ function phrases(line){
   }
 }
 lines = lines.filter(phrases);
-console.log(lines);
 console.log(lines.length);
+
+
+// --------------- Part Two ----------------
+// New Rules:
+// 1. Any two letters appear atleast twice. No Overlapping
+//   ie: xyxy OR aabejioeaajfioe NOT aaa (overlaps)
+// 2. Contains one letter that repeats with one letter in between
+//   ie: aya OR jfioeafajfioe
+
+// Test Two: Letter DifLetter Letter
+function sandwich(line){
+  line = line.split('');
+  for (var i = 0; i < line.length; i++) {
+    if (line[i] === line[i + 2]) {
+      return line.join('');
+    }
+  }
+}
+
+lines2 = lines2.filter(sandwich);
+console.log(lines2);
+
+// Test One: two letters twice.
+function letters(line){
+  var count = 0;
+  var twice = line.indexOf('th');
+  while (twice !== -1) {
+    count ++
+    twice = line.indexOf('th', twice + 1)
+  }
+  return count;
+}
+
+console.log(letters('rthkunfaakmwtmush'));
